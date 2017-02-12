e09d8186bd419b2e6945c721173b72af2aa1de0d
Matias Insaurralde <matias@insaurral.de>
2017-02-12 03:10:17 +0200

    Add the benchmark program.

---
+++ bench.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <benchmark/benchmark.h>
+#include <benchmark/benchmark_api.h>
+#include <benchmark/macros.h>
+#include <benchmark/reporter.h>
+
+#include <google/protobuf/arena.h>
+
+#include "out/person.pb.h"
+#include "out/lite_person.pb.h"
+
+google::protobuf::Arena the_arena;
+
+void BM_perform_serialization(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    Person p;
+
+    std::string name("john");
+    std::string email("john@test.com");
+
+    p.set_name(name);
+    p.set_id(1);
+    p.set_email(email);
+
+    std::string output;
+    p.SerializeToString(&output);
+  };
+};
+
+void BM_perform_lite_serialization(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    LitePerson p;
+
+    std::string name("john");
+    std::string email("john@test.com");
+
+    p.set_name(name);
+    p.set_id(1);
+    p.set_email(email);
+
+    std::string output;
+    p.SerializeToString(&output);
+  };
+};
+
+void BM_perform_arena_serialization(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    Person* p = google::protobuf::Arena::CreateMessage<Person>(&the_arena);
+
+    std::string name("john");
+    std::string email("john@test.com");
+
+    p->set_name(name);
+    p->set_id(1);
+    p->set_email(email); 
+
+    std::string output;
+    p->SerializeToString(&output);
+  };
+};
+
+void BM_perform_arena_lite_serialization(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    LitePerson* p = google::protobuf::Arena::CreateMessage<LitePerson>(&the_arena);
+
+    std::string name("john");
+    std::string email("john@test.com");
+
+    p->set_name(name);
+    p->set_id(1);
+    p->set_email(email); 
+
+    std::string output;
+    p->SerializeToString(&output);
+  };
+};
+
+BENCHMARK(BM_perform_serialization);
+BENCHMARK(BM_perform_lite_serialization);
+BENCHMARK(BM_perform_arena_serialization);
+BENCHMARK(BM_perform_arena_lite_serialization);
+
+BENCHMARK_MAIN();
